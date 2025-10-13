416dbf277ddc96d3ed9141f4f5c4b6c629d602b5
gokul <gokuldas.n@gmail.com>
2025-10-13 16:30:40 +0530

    Fix SQL syntax in get_advances for PostgreSQL

--- services/advances.py
+++ services/advances.py
@@ -21,26 +21,14 @@ def add_advance(trip_id, payer_id, receiver_id, amount, date):
         conn.close()
 
 
-def get_advances(trip_id):
+def add_advance(trip_id, payer_id, receiver_id, amount, date):
     conn = get_connection()
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-
+    cursor = conn.cursor()
     cursor.execute("""
-        SELECT 
-            a.id,
-            a.amount,
-            a.date,
-            f1.family_name AS payer_name,
-            f2.family_name AS receiver_name
-        FROM advances a
-        LEFT JOIN family_details f1 ON a.payer_family_id = f1.id
-        LEFT JOIN family_details f2 ON a.receiver_family_id = f2.id
-        WHERE a.trip_id = %s
-        ORDER BY a.date DESC
-    """, (trip_id,))
-
-    rows = cursor.fetchall()
-    cursor.close()
+        INSERT INTO advances (trip_id, payer_family_id, receiver_family_id, amount, date)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (trip_id, payer_id, receiver_id, amount, date))
+    conn.commit()
     conn.close()
+    return {"message": "Advance recorded successfully"}
 
-    return {"advances": rows}
